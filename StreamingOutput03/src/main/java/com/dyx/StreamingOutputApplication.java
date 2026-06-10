da1e5d9a6bf62fbc00e4d2abb70e9592427bdbdd
codeYx <13190416497@163.com>
2026-06-11 00:15:55 +0800

    修改

---
+++ StreamingOutput03/src/main/java/com/dyx/StreamingOutputApplication.java
@@ -0,0 +1,14 @@
+package com.dyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StreamingOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingOutputApplication.class, args);
+    }
+
+}
---
+++ StreamingOutput03/src/main/java/com/dyx/config/SaaLLMConfig.java
@@ -0,0 +1,50 @@
+package com.dyx.config;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 多模型共存
+ */
+@Configuration
+public class SaaLLMConfig
+{
+    // 模型名称常量定义
+    private final String DEEPSEEK_MODEL = "deepseek-v4-flash";
+    private final String QWEN_MODEL = "qwen3-max";
+    @Value("${spring.ai.dashscope.api-key}")
+    private String apiKey;
+
+    @Bean(name = "deepseek")
+    public ChatModel deepSeek()
+    {
+        return DashScopeChatModel.builder()
+                .dashScopeApi(DashScopeApi.builder()
+                        .apiKey(apiKey)
+                        .build())
+                .defaultOptions(
+                        DashScopeChatOptions.builder().withModel(DEEPSEEK_MODEL).build()
+                )
+                .build();
+    }
+
+    @Bean(name = "qwen")
+    public ChatModel qwen()
+    {
+        return DashScopeChatModel.builder().dashScopeApi(DashScopeApi.builder()
+                        .apiKey(apiKey)
+                        .build())
+                .defaultOptions(
+                        DashScopeChatOptions.builder()
+                                .withModel(QWEN_MODEL)
+                                .build()
+                )
+                .build();
+    }
+}
