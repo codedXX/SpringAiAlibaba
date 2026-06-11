117744eb4b615727357b171cede0250542c2b6ca
codeYx <13190416497@163.com>
2026-06-11 17:10:57 +0800

    向量化和向量数据库Embed2vector10

---
+++ Embed2vector10/src/main/java/com/dyx/Embed2vectorApplication.java
@@ -0,0 +1,14 @@
+package com.dyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Embed2vectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Embed2vectorApplication.class, args);
+    }
+
+}
