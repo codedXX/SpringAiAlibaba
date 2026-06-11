3bd9a79739d2960d6f0ee20ca7f5b15a66850c1b
codeYx <13190416497@163.com>
2026-06-11 15:47:34 +0800

    StructuredOutput06

---
+++ StructuredOutput06/src/main/java/com/dyx/StructuredOutputApplication.java
@@ -0,0 +1,14 @@
+package com.dyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StructuredOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredOutputApplication.class, args);
+    }
+
+}
---
+++ StructuredOutput06/src/main/java/com/dyx/records/StudentRecord.java
@@ -0,0 +1,6 @@
+package com.dyx.records;
+
+/**
+ * @Description jdk14后的新特性，记录类替代lombok
+ */
+public record StudentRecord(String id,String sname,String major,String email) { }
