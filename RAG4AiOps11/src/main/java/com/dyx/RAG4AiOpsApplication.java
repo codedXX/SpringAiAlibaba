f4c46c7355c32142174197bb55a88d225a79955b
codeYx <13190416497@163.com>
2026-06-11 17:45:59 +0800

    RAG4AiOps11

---
+++ RAG4AiOps11/src/main/java/com/dyx/RAG4AiOpsApplication.java
@@ -0,0 +1,14 @@
+package com.dyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RAG4AiOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RAG4AiOpsApplication.class, args);
+    }
+
+}
