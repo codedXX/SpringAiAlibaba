bc328d1e82f34615540f05f43243a086ba72dab8
codeYx <13190416497@163.com>
2026-06-11 14:57:55 +0800

    修改

---
+++ PromptTemplate05/src/main/java/com/dyx/controller/PromptTemplateController.java
@@ -0,0 +1,91 @@
+package com.dyx.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class PromptTemplateController {
+    @Resource(name = "deepseek")
+    private ChatModel deepseekChatModel;
+    @Resource(name = "qwen")
+    private ChatModel qwenChatModel;
+
+    @Resource(name = "deepseekChatClient")
+    private ChatClient deepseekChatClient;
+    @Resource(name = "qwenChatClient")
+    private ChatClient qwenChatClient;
+    @Value("classpath:/prompttemplate/atguigu-template.txt")
+    private org.springframework.core.io.Resource userTemplate;
+
+    /**
+     * @Description: PromptTemplate基本使用，使用占位符设置模版 PromptTemplate
+     * 测试地址
+     * http://localhost:8006/prompttemplate/chat?topic=java&output_format=html&wordCount=200
+     */
+    @GetMapping("/prompttemplate/chat")
+    public Flux<String> chat(String topic, String output_format, String wordCount)
+    {
+        PromptTemplate promptTemplate = new PromptTemplate("" +
+                "讲一个关于{topic}的故事" +
+                "并以{output_format}格式输出，" +
+                "字数在{wordCount}左右");
+
+        // PromptTempate -> Prompt
+        Prompt prompt = promptTemplate.create(Map.of(
+                "topic", topic,
+                "output_format",output_format,
+                "wordCount",wordCount));
+
+        return deepseekChatClient.prompt(prompt).stream().content();
+    }
+
+    /**
+     * @Description: PromptTemplate读取模版文件实现模版功能
+     * 测试地址
+     * http://localhost:8006/prompttemplate/chat2?topic=java&output_format=html
+     */
+    @GetMapping("/prompttemplate/chat2")
+    public String chat2(String topic,String output_format)
+    {
+        PromptTemplate promptTemplate = new PromptTemplate(userTemplate);
+
+        Prompt prompt = promptTemplate.create(Map.of("topic", topic, "output_format", output_format));
+
+        return deepseekChatClient.prompt(prompt).call().content();
+    }
+
+    /**
+     * @Description:
+     * 系统消息(SystemMessage)：设定AI的行为规则和功能边界(xxx助手/什么格式返回/字数控制多少)。
+     * 用户消息(UserMessage)：用户的提问/主题
+     * http://localhost:8006/prompttemplate/chat3?sysTopic=法律&userTopic=知识产权法
+     *
+     * http://localhost:8006/prompttemplate/chat3?sysTopic=法律&userTopic=夫妻肺片
+     */
+    @GetMapping("/prompttemplate/chat3")
+    public String chat3(String sysTopic, String userTopic)
+    {
+        // 1.SystemPromptTemplate
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate("你是{systemTopic}助手，只回答{systemTopic}其它无可奉告，以HTML格式的结果。");
+        Message sysMessage = systemPromptTemplate.createMessage(Map.of("systemTopic", sysTopic));
+        // 2.PromptTemplate
+        PromptTemplate userPromptTemplate = new PromptTemplate("解释一下{userTopic}");
+        Message userMessage = userPromptTemplate.createMessage(Map.of("userTopic", userTopic));
+        // 3.组合【关键】 多个 Message -> Prompt
+        Prompt prompt = new Prompt(List.of(sysMessage, userMessage));
+        // 4.调用 LLM
+        return deepseekChatClient.prompt(prompt).call().content();
+    }
+}
